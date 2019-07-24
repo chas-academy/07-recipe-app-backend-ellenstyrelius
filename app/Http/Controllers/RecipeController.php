ed9e18281335f062af51ec7e87bd81b951926aa8
Ellen Styrélius <ellen.styrelius@gmail.com>
2019-07-24 18:11:52 +0200

    Add and modify a THOUSAND times methods for filtering search results from the database, based on written input or chosen diet parameters. Finally getting a decent enough result.

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -16,50 +16,75 @@ class RecipeController extends Controller
         return $recipes;
     }
 
-    public function filter(Request $request)
-    {        
-        $input = $request->query('q');
-        if (str_word_count($input) > 1) {
-        // if ($request->has('q')) {
-            // $search = preg_split('/[\s,]+/', $request->query('q'));
-            $search = preg_split('/[\s,]+/', $input);
-            // if (count($search) > 1) {
-            foreach ($search as $searchWord) {
-                $recipes = Recipe::where('label', 'like', '%' . $searchWord . '%')
-                    ->where('ingredientLines', 'like', '%' . $searchWord . '%')
-                    ->orderBy('label', 'asc')
-                    ->get();
-                foreach ($recipes as $recipe) {
-                    Recipe::decodeJsonStrings($recipe);
-                }
-                return $recipes;
-            }
-            // } else {
-                // $recipes = Recipe::where('label', 'like', '%'.$search.'%')
-                //     ->orWhere('ingredientLines', 'like', '%'.$search.'%')
-                //     ->orderBy('label', 'asc')
-                //     ->get();
-                // foreach ($recipes as $recipe) {
-                //     Recipe::decodeJsonStrings($recipe);
-                // }
-                // return $recipes;
-            // }
-        } elseif (str_word_count($input) === 1) {
-            $recipes = Recipe::where('label', 'like', '%' . $input . '%')
-                ->orWhere('ingredientLines', 'like', '%' . $input . '%')
-                ->orderBy('label', 'asc')
-                ->get();
-            foreach ($recipes as $recipe) {
-                Recipe::decodeJsonStrings($recipe);
-            }
-            return $recipes;
-        }
-    }
-
     public function show($recipe) 
     {
         $recipeData = Recipe::find($recipe);
         $recipeDecoded = Recipe::decodeJsonStrings($recipeData);
         return $recipeDecoded;
+    }
+
+    public function filter(Request $request)
+    {        
+        $input = $request->query('q');
+        $diet = $request->query('diet');
+        $recipes = array();
+    
+        if ($input) {
+
+            if (str_word_count($input) > 1) {
+                $search = explode(' ', $input);
+                foreach ($search as $searchWord) {
+                    $inputRecipes = Recipe::where('label', 'like', '%' . $searchWord . '%')
+                        ->where('ingredientLines', 'like', '%' . $searchWord . '%')
+                        ->orderBy('label', 'asc')
+                        ->get();
+                    foreach ($inputRecipes as $recipe) {
+                        Recipe::decodeJsonStrings($recipe);
+                        $recipes[] = $recipe;
+                    }
+                }
+            } 
+            if (str_word_count($input) === 1) {
+                $inputRecipes = Recipe::where('label', 'like', '%' . $input . '%')
+                    ->orWhere('ingredientLines', 'like', '%' . $input . '%')
+                    ->orderBy('label', 'asc')
+                    ->get();
+                foreach ($inputRecipes as $recipe) {
+                    Recipe::decodeJsonStrings($recipe);
+                    $recipes[] = $recipe;
+                }
+            }
+        }
+
+        if ($diet) {
+            $dietSearch = explode(',', $diet);
+
+            // push the results from both foreach into own arrays and compare them? 
+            // or just compare them as strings? 
+            // this to only get recipes that has all of the labels
+            // then push the result into recipes array
+
+            foreach ($dietSearch as $healthLabel) {
+                $healthRecipes = Recipe::where('healthLabels', 'like', '%' . $healthLabel . '%')
+                    ->orderBy('label', 'asc')
+                    ->get();
+                foreach ($healthRecipes as $recipe) {
+                    Recipe::decodeJsonStrings($recipe);
+                    $recipes[] = $recipe;
+                }  
+            }
+
+            foreach ($dietSearch as $dietLabel) {
+                $dietRecipes = Recipe::where('dietLabels', 'like', '%' . $dietLabel . '%')
+                    ->orderBy('label', 'asc')
+                    ->get();
+                foreach ($dietRecipes as $recipe) {
+                    Recipe::decodeJsonStrings($recipe);
+                    $recipes[] = $recipe;
+                }
+            }
+        }
+
+        return $recipes;
     }
 }
