a66a85983619147b9ab78715eed52c9a8b9e0de7
Ellen Styrélius <ellen.styrelius@gmail.com>
2019-07-30 13:15:00 +0200

    Add AuthController with methods to register, login, logout and get an authenticated user. Modify user model and migration to match the AuthController.

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'email', 'password',
     ];
 
     /**
@@ -31,6 +31,8 @@ class User extends Authenticatable
 
     public function savedRecipes()
     {
+
+        // or is it belongsToMany??????
         return $this->hasMany(SavedRecipe::class);
     }
 }
